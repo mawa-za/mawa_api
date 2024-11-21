8e8d4ecc6653c0d1a24733e58fa10253fe7f09da
tebogo.mohale <tebogo.mohale@raretag.co.za>
2024-11-21 13:16:38 +0200

    MP-14 - User should be able to create a receipt

--- lib/src/keys/network-resource-keys.dart
+++ lib/src/keys/network-resource-keys.dart
@@ -2,6 +2,7 @@ class Resources {
   static const String authenticate = 'authenticate';
   static const String membership = 'membership';
   static const String membershipV2 = 'membership/v2';
+  static const String partner = 'partner';
   static const String purchaseOrder = 'purchase-order';
   static const String salesOrder = 'sales-order';
   static const String invoice = 'invoice';
---
+++ lib/src/services/individual-service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:mawa_api/src/keys/network-resource-keys.dart';
+import 'package:mawa_api/src/objects/individual.dart';
+import 'package:mawa_api/src/services/network-request.dart';
+
+class IndividualService {
+  getAll() async {
+    dynamic jsonObject;
+    List<Individual> objectList = [];
+    dynamic response = await NetworkRequest().securedMawaAPI(
+        NetworkRequest.methodGet,
+        resource: Resources.partner);
+
+    if (response.statusCode == 200) {
+      jsonObject = jsonDecode(response.body);
+      objectList = (jsonObject as List)
+          .map((data) => Individual.fromJson(data))
+          .toList();
+    } else {}
+    return objectList;
+  }
+
+  get(String id) async {
+    dynamic jsonObject;
+    List<Individual> objectList = [];
+    dynamic response = await NetworkRequest().securedMawaAPI(
+        NetworkRequest.methodGet,
+        resource: '${Resources.partner}/$id');
+
+    if (response.statusCode == 200) {
+      jsonObject = jsonDecode(response.body);
+      objectList = (jsonObject as List)
+          .map((data) => Individual.fromJson(data))
+          .toList();
+    } else {}
+    return objectList;
+  }
+}
