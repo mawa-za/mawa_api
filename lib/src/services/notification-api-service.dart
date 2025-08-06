45d9c6270127896dea52d86273eadef8f5822705
TebogoMohale <tebogo.mohale@mawa.co.za>
2025-08-06 23:20:14 +0200

    add notification service

--- lib/src/services/notification-api-service.dart
+++ lib/src/services/notification-api-service.dart
@@ -1,22 +1,13 @@
 import 'dart:convert';
 import 'package:mawa_api/src/keys/network-resource-keys.dart';
-import 'package:mawa_api/src/objects/notification.dart';
 import 'package:mawa_api/src/services/network-request.dart';
 
-class NotificationApiService{
+class NotificationApiService {
   get(String user, String status) async {
-    dynamic jsonObject;
-    List<Notification> notificationObjectList = [];
     dynamic response = await NetworkRequest().securedMawaAPI(
         NetworkRequest.methodGet,
         resource: '${Resources.notification}',
-        queryParameters: {'user': user, 'status':status});
-
-    if (response.statusCode == 200) {
-      jsonObject = jsonDecode(response.body);
-      notificationObjectList =
-          (jsonObject as List).map((data) => Notification.fromJson(data)).toList();
-    } else {}
-    return notificationObjectList;
+        queryParameters: {'user': user, 'status': status});
+    return response;
   }
 }
